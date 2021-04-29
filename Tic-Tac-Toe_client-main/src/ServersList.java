6583715cb27856b5ab5aff50b0da89499d3d86a0
DimonLukhtanov <36221895+Dimonlukhtanov@users.noreply.github.com>
2021-04-30 01:46:52 +0700

    start changes

---
+++ Tic-Tac-Toe_client-main/src/ServersList.java
@@ -0,0 +1,10 @@
+public class ServersList {
+
+    final static String[][] ServersAddresses = new String[][]{
+            {"localhost", "5455"},
+            {"localhost", "5456"},
+            {"localhost", "5457"}
+    };
+
+    static boolean valid[] = new boolean[ServersAddresses.length];
+}
---
+++ Tic-Tac-Toe_server-main/src/ServersAddresses.java
@@ -0,0 +1,7 @@
+public class ServersAddresses {
+    final static String[][] Addresses = new String[][]{
+            {"localhost", "5555"},
+            {"localhost", "5556"},
+            {"localhost", "5557"}
+    };
+}
